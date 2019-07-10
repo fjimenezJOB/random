08f3c5705d9b3f667ef012a352e245fb64637ed4
jaumecosta <jaumecostagomez@gmail.com>
2019-07-10 12:37:21 +0200

    comit de Jaume para fran

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
 // rand();
 // rand(min,max);
 // ************************************************
-
 <?php
+$nombres = ['toni','jorge', 'julia', 'jose', 'mateo', 'jaume', 'petro', 'alejandro', 'fran', 'adrian', 'lolo', 'cristian','jordi', 'david', 'luis', 'alex'];
+?>
+<?php
+
 echo (rand(10, 30) . "<br>");
 echo (rand(1, 1000000) . "<br>");
 echo (rand());
