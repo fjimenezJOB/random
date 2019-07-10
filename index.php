330adae2ae0ccdbb3a5c0df87c74af95a921461f
jaumecosta <jaumecostagomez@gmail.com>
2019-07-10 11:51:07 +0200

    segundo commit

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+// Dos maneras de hacer Random:
+// La primera un número aleatorio
+// la segunda un número entre el mínimo y el máximo
+// rand();
+// rand(min,max);
+// ************************************************
+
 <?php
-echo("comunicaciom");
+echo (rand(10, 30) . "<br>");
+echo (rand(1, 1000000) . "<br>");
+echo (rand());
 ?>
+
+// Resultado
+// 20
+// 442549
+// 830380191
